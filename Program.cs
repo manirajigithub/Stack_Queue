6e377aabb5cc0fa23212ff06c39686c3cc127c0a
mani1 <mani1@NANI143>
2022-08-12 11:27:57 +0530

    [Rajeswari]Added Program .cs

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace StackProblemUC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Stack");
+            Stack stack1 = new Stack();
+            stack1.StackPush(70);
+            stack1.StackPush(30);
+            stack1.StackPush(56);
+            stack1.Peak();
+            stack1.Display();
+
+        }
+    }
+}
+
+           
+
