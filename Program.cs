334e6d404fa706f0b0701b9e9bf0409d596c83de
mani1 <mani1@NANI143>
2022-08-12 11:15:31 +0530

    [Rajeswari]Added Program.cs

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace StackProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Stack and Queue");
+
+
+
+            Stack stack = new Stack();
+            stack.StackPush(70);
+            stack.StackPush(30);
+            stack.StackPush(56);
+            stack.DisplayStack();
+        }
+    }
+}
+
+               
