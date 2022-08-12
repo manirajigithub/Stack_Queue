7c7951dd321d41e87fe2137347cc35ab3d0fa20e
mani1 <mani1@NANI143>
2022-08-12 11:47:26 +0530

    [Rajeswari]Added Program.cs

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace QueueProblemUC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue queue = new Queue();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            queue.DisplayQueue();
+            queue.Dequeue();
+            queue.DisplayQueue();
+        }
+    }
+}
